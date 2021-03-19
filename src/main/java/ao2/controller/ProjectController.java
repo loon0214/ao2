336f6d343d4f1a284211a72a2c4ffaad746a3330
gridone <gridone@DESKTOP-CT1G8N9>
2021-03-19 14:14:48 +0900

    ,

--- src/main/java/ao2/controller/ProjectController.java
+++ src/main/java/ao2/controller/ProjectController.java
@@ -6,6 +6,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,17 +27,29 @@ public class ProjectController {
 		System.out.println("project List");
 		return mv;
 	}
-	
-	@RequestMapping("project/projectPost")
-	private String projectPostForm() {
-		System.out.println("project post");
-		return "project/projectPost";
-	}
-	
+		
+//	@RequestMapping("project/projectPost")
+//	private String projectPostForm() {
+//		System.out.println("project post");
+//		return "project/projectPost";
+//	}
+//	
 	@RequestMapping("/projectPostProc")
 	private int projectPostProc(HttpServletRequest request) throws Exception{
 		Project project = (Project) request.getParameterMap();
 		System.out.println("project post process");
 		return service.projectPostService(project);
+	}
+	
+	@RequestMapping("project/projectPost")
+	public String projectPost(Model model) {
+		
+		String pj_id = "제목";
+		String pj_content = "내용";
+		
+		model.addAttribute("t", pj_id);
+		model.addAttribute("c", pj_content);
+		
+		return "project/projectPost";
 	}
 }
